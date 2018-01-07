677f176b1ded84bcbd1cd74c0907e3183f753789
lo52_bishop <lo52@utbm.fr>
2018-01-08 00:35:56 +0100

    Suppression de fonctions inutiles

--- OpencvTest2/Array.h
+++ OpencvTest2/Array.h
@@ -3,9 +3,5 @@
 #include "Include.h"
 #include "Constants.h"
 
-// Sort array
-void swap(Point2d tableau[], int a, int b);
-void sortTabByY(Point2d tableau[], int debut, int fin);
-
 // Vector contain
 bool vectorContains(vector<int> tab, int value);
--- OpencvTest2/Constants.h
+++ OpencvTest2/Constants.h
@@ -23,7 +23,6 @@ const string XML_PATH = "xml";
 const string TRAINING_PICTURES_PATH = "TrainingPictures2";
 
 // Constant filename
-const string XML_PERIMETER_FILE_NAME = "perimeters.xml";
 const string XML_CONTOUR_TABLE_FILE_NAME = "insideContourTable.xml";
 const string XML_KNN_IMAGES_FILE_NAME = "images.xml";
 const string XML_KNN_CLASSIFICATION_FILE_NAME = "classification.xml";
@@ -31,12 +30,6 @@ const string XML_KNN_CLASSIFICATION_FILE_NAME = "classification.xml";
 // Constant to filter size of polygons' detected
 const int TOLERANCE_VALUE = 60;
 const int TOLERANCE_INTERN_VALUE = 30;
-
-// Search area in pourcentage of picture's size
-const int SEARCH_AREA_LEFT = 30;
-const int SEARCH_AREA_RIGHT = 60;
-const int SEARCH_AREA_TOP = 30;
-const int SEARCH_AREA_BOTTOM = 65;
 
 // K parameter for KNN range algorithm : nb of nearest matches in range
 const int KNN_K_PARAMETER = 1;
